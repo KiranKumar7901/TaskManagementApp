2f4d300243d0268e987ae8dd06f0ac6a1ddd8cb0
Kiran Kumar <kkp792001@gmail.com>
2024-09-26 02:37:19 +0530

    files changes

--- frontend/src/pages/CompletedTask.jsx
+++ frontend/src/pages/CompletedTask.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Cards from '../components/Home/Cards'
 
 const CompletedTask = () => {
   return (
     <div>
-      Completed Tasks
+      <Cards home="false"/>
     </div>
   )
 }
--- frontend/src/pages/InCompleteTask.jsx
+++ frontend/src/pages/InCompleteTask.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Cards from '../components/Home/Cards'
 
 const InCompleteTask = () => {
   return (
     <div>
-      Incomplete Tasks
+      <Cards home="false"/>
     </div>
   )
 }
